a71ef29dc919ae2ddd805b7f69bbfa180c5fbe90
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2023-04-10 00:11:17 +0530

    final commit

--- contours.py
+++ contours.py
@@ -18,6 +18,10 @@ ret , thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
 print(f'ret = ',ret)
 cv.imshow('thresh',thresh)
 
+# 📌 cv.RETR_LIST: This argument specifies the retrieval mode for the contours. RETR_LIST retrieves all of the contours without any hierarchical information.
+
+# 📌 contours: This is a Python list that contains all the contours found in the image. Each contour is represented by a numpy array of (x,y) coordinates.
+
 # contours ,hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 # print(f'{len(contours)} contour(s) found using simple canny edges')
 
